326551ea24175e40e65c2a40fc1e382fae4f10ff
mckay <simi7236@gmail.com>
2020-06-21 07:56:12 +0900

    Scroll to Div

--- script/Portfolio.js
+++ script/Portfolio.js
@@ -1,14 +1,20 @@
-var nav = $("#dot ul li");
-var contents = $("#contents > div");
+let nav = $("#dot ul li");
+let contents = $("#contents > div");
 
 nav.click(function(e){
     e.preventDefault();
-    var target = $(this);
-    var index = target.index();
-    var section = contents.eq(index);
-    var offset = section.offset().top;
+    let target = $(this);
+    let index = target.index();
+    let section = contents.eq(index);
+    let offset = section.offset().top;
 
-    $("html,body").animate({scrollTop:offset},600,"easeInOutExpo");
+    $("html,body").animate({
+        scrollTop: offset},
+        600,
+        "easeInOutExpo");
+    // $('html, body').animate({
+    //     scrollTop: $('.container').offset().top
+    // }, 'fast');
 });
 
 
@@ -39,4 +45,58 @@ $(window).scroll(function(){
         nav.removeClass("active");
         nav.eq(5).addClass("active");
     }
+});
+
+
+$(document).on("ready", function () {
+    $("#contents > div").each(function () {
+        $(this).on("mousewheel DOMMouseScroll", function (event) {
+            let delta = 0;
+            let moveTop = null;
+            let boxMax = $("#contents > div").length - 1;
+
+            if (event) {
+                event = window.event;
+            }
+
+            if (event.wheelDelta) {
+                delta = event.wheelDelta / 120;
+                if(window.opera) {
+                    delta = -delta;
+                }
+            }
+            else if (window.detail) {
+                delta = -event.detail / 3;
+            }
+
+            if (delta < 0) {
+                if ($(this).index() < boxMax) {
+                    if($(this).next() !== undefined) {
+                        moveTop = $(this).next().offset().top;
+                    }
+                }
+                else {
+                    // alert("마지막 페이지 입니다");
+                    return false;
+                }
+            }
+            else {
+                if ($(this).index() > 0) {
+                    if ($(this).prev() !== undefined) {
+                        moveTop = $(this).prev().offset().top;
+                    }
+                }
+                else {
+                    // alert("첫번째 페이지 입니다");
+                    return false;
+                }
+            }
+            $("html, body").stop().animate({
+                    scrollTop: moveTop + "px"},
+                {
+                    duration: 600,
+                    composite: function () {}
+                });
+        });
+    });
 });
