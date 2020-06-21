715360ad0068035b206ff46b53cf964a35f97b96
mckay <simi7236@gmail.com>
2020-06-21 21:48:54 +0900

    Scroll to Div Complete

--- script/test.js
+++ script/test.js
@@ -9,7 +9,7 @@ nav.click(function(e){
     let offset = section.offset().top;
 
     $("html,body").animate({
-        scrollTop: offset},
+            scrollTop: offset},
         600,
         "easeInOutExpo");
     // $('html, body').animate({
@@ -50,64 +50,53 @@ $(window).scroll(function(){
 
 $(document).on("ready", function () {
     $("#contents > div").each(function () {
-        // mousewheel: 크롬 익스 사파리 오페라
-        // DOMMouseScroll: 파이어폭스
-        // 두개의 이벤트를 동시에 수행하는 on을 사용
-        $(this).on("mousewheel DOMMouseScroll", function (event) {
-            let delta = 0;
-            let moveTop = null;
-            let boxMax = $("#contents > div").length - 1;
+        $(this).on("mousewheel DOMMouseScroll", function(event) {
+            var delta = 0;
+            var moveTop = null;
+            var boxMax = $("#contents > div").length - 1;
+            var winEvent = "";
 
-            if (event) {
-                event = window.event;
+            if(!winEvent) {
+                winEvent = window.event;
             }
 
-            if (event.wheelDelta) {
-                // mousewheel은
-                // 휠을 내릴 떄 -120 반환
-                // 휠을 올릴 때 120 반환
-                // 그래서 1이나 -1로 반환하도록 / 120
-                delta = event.wheelDelta / 120;
+            if(winEvent.wheelDelta) {
+                delta = winEvent.wheelDelta / 120;
                 if(window.opera) {
                     delta = -delta;
                 }
             }
-            else if (window.detail) {
-                // 파이어폭스의 속성
-                // 휠을 내릴 때 3 반환
-                // 휠을 올릴 때 -3 반환
-                // 위의 delta값과 부호와 수가 다름. 그래서 1로 만들도록
-                // - 하고 나누기 3
-                delta = -event.detail / 3;
+            else if(winEvent.detail) {
+                delta = -winEvent.detail / 3;
             }
 
-            if (delta < 0) {
-                if ($(this).index() < boxMax) {
+            if(delta < 0) {
+                if($(this).index() < boxMax) {
                     if($(this).next() !== undefined) {
                         moveTop = $(this).next().offset().top;
                     }
                 }
                 else {
-                    // alert("마지막 페이지 입니다");
+                    alert("마지막 페이지 입니다.");
                     return false;
                 }
             }
             else {
-                if ($(this).index() > 0) {
-                    if ($(this).prev() !== undefined) {
+                if($(this).index() > 0) {
+                    if($(this).prev() !== undefined) {
                         moveTop = $(this).prev().offset().top;
                     }
                 }
                 else {
-                    // alert("첫번째 페이지 입니다");
+                    alert("첫번째 페이지 입니다.");
                     return false;
                 }
             }
-            $("html, body").stop().animate({
-                    scrollTop: moveTop + "px"},
+            $("html,body").stop().animate({
+                    scrollTop : moveTop + "px"},
                 {
                     duration: 600,
-                    complete: function () {}
+                    complete : function () {}
                 });
         });
     });
